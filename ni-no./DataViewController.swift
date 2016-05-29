ac256d4f81d6102bc6448b218253d2f9cb42a82d
玉井　孝治 <Koji@tamaikouji-no-macbook-air.local>
2016-05-29 15:20:09 +0900

    Initial Commit

---
+++ ni-no./DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  ni-no.
+//
+//  Created by 玉井　孝治 on 2016/05/29.
+//  Copyright © 2016年 Koji TAMAI, Taro UENO. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
